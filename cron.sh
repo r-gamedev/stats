61b1bf5343620332a5e2446f9efef5df82b66c73
Lemtzas <lemtzas@gmail.com>
2016-01-09 18:17:03 -0800

    update cron script

--- cron.sh
+++ cron.sh
@@ -5,11 +5,17 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo running in $DIR
+cd $DIR
+
+git checkout r-gamedev
+git pull
 
 # load rvm ruby
 source $(rvm env --path -- ruby 2.2.1)
 
-cd $DIR
-
 bundle install
 bundle exec ruby run.rb
+
+git add -A
+git commit -am "Stats update."
+
