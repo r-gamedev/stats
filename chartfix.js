e1a8c680604708cb9173d19807c8f2eb2f104af7
Lemtzas <lemtzas@gmail.com>
2016-01-16 18:45:22 -0800

    adding first generation

---
+++ chartfix.js
@@ -0,0 +1,39 @@
+var tooltipFunction = function () {
+  var s = '<b>' + this.x + '</b>';
+  var sortedPoints = this.points.sort(function(a, b){
+    return ((a.y > b.y) ? -1 : ((a.y < b.y) ? 1 : 0));
+  });
+
+  $.each(sortedPoints, function () {
+    s += '<br/><span style="color:'+ this.series.color +'">\u25CF</span> ' + this.series.name + ': ' +
+    this.y + 'm';
+  });
+
+  return s;
+}
+
+function chartfix() {
+  try {
+    $($("[id^=chart]").first()).highcharts()
+  } catch (err) {
+    console.log("failed to update highcharts")
+    setTimeout(chartfix, 500);
+    return;
+  }
+
+  $("[id^=chart]").each(function (e) {
+    try {
+      $(this).highcharts().options.tooltip.formatter = tooltipFunction;
+    } catch (err) {
+      debugger;
+      console.log("failed to update highcharts")
+      return;
+    }
+  })
+  console.log("chartfix.js: Highcharts updated.");
+}
+
+$().ready(function() {
+  chartfix();
+})
+// $("#chart-3").highcharts().series[0].update(
